6185f5f885d8d34b4c046112127798cfba58aa7a
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2024-11-06 21:55:58 +0530

    Create Multiplication.java

---
+++ Oops-java/Tutorial-java/Cycle-2/Multiplication.java
@@ -0,0 +1,21 @@
+//program to print multiplication table
+//sahil
+//Roll no: 54
+import java.util.*;
+class multiplication
+{
+	public static void main(String args[])
+	{
+		Scanner S=new Scanner(System.in);
+		System.out.println("enter the number :");
+		int n= S.nextInt();
+		System.out.println("enter the range:");
+		int m=S.nextInt();
+		System.out.println("the multiplication table:");
+		for(int i=0;i<=m;i++)
+		{	
+			System.out.println(n+"x"+i+"="+n*i);
+		}
+		S.close();
+	}
+}
