d52cbeb77d1d90ac9be139281ba1692b4e34f37a
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2024-11-06 14:55:46 +0530

    Update javasynchronization.java

--- Oops-java/Java-Lab/cycle_5/javasynchronization.java
+++ Oops-java/Java-Lab/cycle_5/javasynchronization.java
@@ -53,3 +53,21 @@ class bank
    	}
   }
 
+student@ml-23:~/sahil$ javac synch.java
+student@ml-23:~/sahil$ java bank
+Customer 1Deposite amount:100NewBalance:1100
+Customer 1Withdraw200NewBalance900
+Customer 1Deposite amount:100NewBalance:1000
+Customer 1Withdraw200NewBalance800
+Customer 1Deposite amount:100NewBalance:900
+Customer 1Withdraw200NewBalance700
+Customer 2Deposite amount:100NewBalance:800
+Customer 2Withdraw200NewBalance600
+Customer 2Deposite amount:100NewBalance:700
+Customer 2Withdraw200NewBalance500
+Customer 2Deposite amount:100NewBalance:600
+Customer 2Withdraw200NewBalance400
+student@ml-23:~/sahil$ 
+
+
+
