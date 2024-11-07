8375e2128d5fdbd3ba7506ac565fe8d0f71c8e59
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2024-11-07 11:11:56 +0530

    Create Indixtopost.c

---
+++ DS-LAB/Infixtopostfix/Indixtopost.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+int isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+int precedence(char c) {
+    switch (c) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
+    }
+}
+
+
+int isOperand(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+}
+
+void infixToPostfix(char* infix, char* postfix) {
+    char stack[100];
+    int top = -1;
+    int i = 0;   
+    int j = 0;    
+
+    while (infix[i] != '\0') {
+        if (isOperand(infix[i])) {
+            postfix[j++] = infix[i];
+        } else if (infix[i] == '(') {
+            stack[++top] = infix[i];
+        } else if (infix[i] == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = stack[top--];
+            }
+            top--; 
+        } else if (isOperator(infix[i])) {
+            while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
+                postfix[j++] = stack[top--];
+            }
+            stack[++top] = infix[i];
+        }
+        i++;
+    }
+
+    while (top != -1) {
+        postfix[j++] = stack[top--];
+    }
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[100], postfix[100];
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
