79b6e2419ba6cf45018189937e2735ef8d40105e
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2025-01-04 11:53:12 +0530

    Update date.sh

--- DS-LAB/bash/date.sh
+++ DS-LAB/bash/date.sh
@@ -55,3 +55,148 @@ student@dl-23:~/sahilkhn$
 
 
 
+student@dl-25:~/sakhil$ date
+Saturday 04 January 2025 11:23:42 AM IST
+student@dl-25:~/sakhil$ date +%m
+01
+student@dl-25:~/sakhil$ date +%h
+Jan
+student@dl-25:~/sakhil$ date +%d
+04
+student@dl-25:~/sakhil$ date +%y
+25
+student@dl-25:~/sakhil$ date +%H
+11
+student@dl-25:~/sakhil$ date +%M
+24
+student@dl-25:~/sakhil$ date +%S
+20
+student@dl-25:~/sakhil$ cal 2025
+                            2025
+      January               February               March          
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
+          1  2  3  4                     1                     1  
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   2  3  4  5  6  7  8  
+12 13 14 15 16 17 18   9 10 11 12 13 14 15   9 10 11 12 13 14 15  
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  16 17 18 19 20 21 22  
+26 27 28 29 30 31     23 24 25 26 27 28     23 24 25 26 27 28 29  
+                                            30 31                 
+
+       April                  May                   June          
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
+       1  2  3  4  5               1  2  3   1  2  3  4  5  6  7  
+ 6  7  8  9 10 11 12   4  5  6  7  8  9 10   8  9 10 11 12 13 14  
+13 14 15 16 17 18 19  11 12 13 14 15 16 17  15 16 17 18 19 20 21  
+20 21 22 23 24 25 26  18 19 20 21 22 23 24  22 23 24 25 26 27 28  
+27 28 29 30           25 26 27 28 29 30 31  29 30                 
+                                                                  
+
+        July                 August              September        
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
+       1  2  3  4  5                  1  2      1  2  3  4  5  6  
+ 6  7  8  9 10 11 12   3  4  5  6  7  8  9   7  8  9 10 11 12 13  
+13 14 15 16 17 18 19  10 11 12 13 14 15 16  14 15 16 17 18 19 20  
+20 21 22 23 24 25 26  17 18 19 20 21 22 23  21 22 23 24 25 26 27  
+27 28 29 30 31        24 25 26 27 28 29 30  28 29 30              
+                      31                                          
+
+      October               November              December        
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
+          1  2  3  4                     1      1  2  3  4  5  6  
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   7  8  9 10 11 12 13  
+12 13 14 15 16 17 18   9 10 11 12 13 14 15  14 15 16 17 18 19 20  
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  21 22 23 24 25 26 27  
+26 27 28 29 30 31     23 24 25 26 27 28 29  28 29 30 31           
+                      30                                          
+student@dl-25:~/sakhil$ echo"hlo"
+echohlo: command not found
+student@dl-25:~/sakhil$ echo "hlo"
+hlo
+student@dl-25:~/sakhil$ ls -l
+total 8
+-rw-rw-r-- 1 student student 18 Jan  4 11:11 fact.c
+---------- 1 student student 12 Jan  4 11:09 sum.c
+student@dl-25:~/sakhil$ ls -t
+fact.c  sum.c
+student@dl-25:~/sakhil$ ls -u
+fact.c  sum.c
+student@dl-25:~/sakhil$ ls -s
+total 8
+4 fact.c  4 sum.c
+student@dl-25:~/sakhil$ ls r
+ls: cannot access 'r': No such file or directory
+student@dl-25:~/sakhil$ ls -r
+sum.c  fact.c
+student@dl-25:~/sakhil$ ls -f
+..  .  fact.c  sum.c
+student@dl-25:~/sakhil$ ls -S
+fact.c  sum.c
+student@dl-25:~/sakhil$ ls -h
+fact.c  sum.c
+student@dl-25:~/sakhil$ ls -[a-g]*
+ls: invalid option -- '['
+Try 'ls --help' for more information.
+student@dl-25:~/sakhil$ ls [a-g]*
+fact.c
+student@dl-25:~/sakhil$ ls [s]*
+sum.c
+student@dl-25:~/sakhil$ man cat
+student@dl-25:~/sakhil$ who$whoami
+student  tty2         2025-01-04 15:23 (tty2)
+student@dl-25:~/sakhil$ uptime
+ 11:29:57 up  1:36,  1 user,  load average: 0.11, 0.07, 0.01
+student@dl-25:~/sakhil$ uname -a
+Linux dl-25 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
+student@dl-25:~/sakhil$ hostname
+dl-25
+student@dl-25:~/sakhil$ cat >palindrome.c
+student@dl-25:~/sakhil$ cat >>palindrome.c
+//palindrome
+student@dl-25:~/sakhil$ cat palindrome.c
+//palindrome
+student@dl-25:~/sakhil$ cat fact.c >>palindrome.c
+student@dl-25:~/sakhil$ cat palindrome.c
+//palindrome
+//sum
+//sum
+//hlo
+student@dl-25:~/sakhil$ grep hlo palindrome.c
+//hlo
+student@dl-25:~/sakhil$ rm sum.c
+rm: remove write-protected regular file 'sum.c'? yes
+student@dl-25:~/sakhil$ ls -t
+palindrome.c  fact.c
+student@dl-25:~/sakhil$ touch sum.c
+student@dl-25:~/sakhil$ ls -t
+sum.c  palindrome.c  fact.c
+student@dl-25:~/sakhil$ cp fact.c sum.c
+student@dl-25:~/sakhil$ cat sum.c
+//sum
+//sum
+//hlo
+student@dl-25:~/sakhil$ mv sum.c sum1.c
+student@dl-25:~/sakhil$ ls -t
+sum1.c  palindrome.c  fact.c
+student@dl-25:~/sakhil$ head -2 palindrome.c
+//palindrome
+//sum
+student@dl-25:~/sakhil$ head palindrome.c
+//palindrome
+//sum
+//sum
+//hlo
+student@dl-25:~/sakhil$ tail -2 palindrome.c
+//sum
+//hlo
+student@dl-25:~/sakhil$ tail palindrome.c
+//palindrome
+//sum
+//sum
+//hlo
+student@dl-25:~/sakhil$ wc -l palindrome.c
+4 palindrome.c
+student@dl-25:~/sakhil$ wc -w palindrome.c
+4 palindrome.c
+student@dl-25:~/sakhil$ wc -c palindrome.c
+31 palindrome.c
+student@dl-25:~/sakhil$ 
