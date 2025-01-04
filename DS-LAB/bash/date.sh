307211d0833dab46bed16f6092846faecd39e939
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2025-01-04 10:18:40 +0530

    Create date.sh

---
+++ DS-LAB/bash/date.sh
@@ -0,0 +1,16 @@
+student@dl-23:~/sahilkhn$ date
+Saturday 04 January 2025 10:10:40 AM IST
+student@dl-23:~/sahilkhn$ date +%m
+01
+student@dl-23:~/sahilkhn$ date +%h
+Jan
+student@dl-23:~/sahilkhn$ date +%d
+04
+student@dl-23:~/sahilkhn$ date +%y
+25
+student@dl-23:~/sahilkhn$ date +%H
+10
+student@dl-23:~/sahilkhn$ date +%M
+13
+student@dl-23:~/sahilkhn$ date +%S
+16
