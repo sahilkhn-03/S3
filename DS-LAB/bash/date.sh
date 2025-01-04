15b1e568b08622b3beb1da6d99e7a7a5c6f72f50
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2025-01-04 10:20:28 +0530

    Update date.sh

--- DS-LAB/bash/date.sh
+++ DS-LAB/bash/date.sh
@@ -14,3 +14,14 @@ student@dl-23:~/sahilkhn$ date +%M
 13
 student@dl-23:~/sahilkhn$ date +%S
 16
+student@dl-23:~/sahilkhn$ cal 01 2025
+    January 2025      
+Su Mo Tu We Th Fr Sa  
+          1  2  3  4  
+ 5  6  7  8  9 10 11  
+12 13 14 15 16 17 18  
+19 20 21 22 23 24 25  
+26 27 28 29 30 31     
+                      
+student@dl-23:~/sahilkhn$ 
+
